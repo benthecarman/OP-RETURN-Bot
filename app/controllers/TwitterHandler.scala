f45da8038e19c10e6606816f59dd17a0d69835e1
Ben Carman <benthecarman@live.com>
2021-06-20 02:37:18 -0500

    Add shill tweets

--- app/controllers/TwitterHandler.scala
+++ app/controllers/TwitterHandler.scala
@@ -5,9 +5,12 @@ import com.danielasfregola.twitter4s.entities.Tweet
 import grizzled.slf4j.Logging
 import org.bitcoins.crypto.DoubleSha256DigestBE
 
+import java.util.concurrent.atomic.AtomicInteger
 import scala.concurrent.Future
 
-trait TwitterHandler extends Logging {
+trait TwitterHandler extends Logging { self: Controller =>
+
+  val shillCounter: AtomicInteger = new AtomicInteger(0)
 
   protected def sendTweet(message: String): Future[Tweet] = {
     val client = TwitterRestClient()
@@ -18,6 +21,12 @@ trait TwitterHandler extends Logging {
   protected def handleTweet(
       message: String,
       txId: DoubleSha256DigestBE): Future[Tweet] = {
+    // Every 15th OP_RETURN we shill
+    val count = shillCounter.getAndIncrement()
+    if (count % 15 == 0 && count != 0) {
+      shillTweet()
+    }
+
     val tweet =
       s"""
          |🔔 🔔 NEW OP_RETURN 🔔 🔔
@@ -28,5 +37,20 @@ trait TwitterHandler extends Logging {
          |""".stripMargin
 
     sendTweet(tweet)
+  }
+
+  protected def shillTweet(): Future[Unit] = {
+    if (uri != uriErrorString) {
+      val tweet =
+        s"""
+           |Like OP_RETURN Bot?
+           |
+           |Consider connecting and opening a lightning channel!
+           |
+           |$uri
+           |""".stripMargin
+
+      sendTweet(tweet).map(_ => ())
+    } else Future.unit
   }
 }
