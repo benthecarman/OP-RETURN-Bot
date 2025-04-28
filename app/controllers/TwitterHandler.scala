096e00a6cb94613d71690ad87f851f8ecfd996ed
benthecarman <benthecarman@live.com>
2025-04-28 12:27:45 -0500

    Remove shill

--- app/controllers/TwitterHandler.scala
+++ app/controllers/TwitterHandler.scala
@@ -4,11 +4,9 @@ import com.github.scribejava.core.model.{OAuthRequest, Verb}
 import grizzled.slf4j.Logging
 import org.bitcoins.core.util.FutureUtil
 import org.bitcoins.crypto.DoubleSha256DigestBE
-import play.api.libs.json.{JsObject, JsValue, Json, Reads}
+import play.api.libs.json.{JsValue, Json, Reads}
 
-import java.util.concurrent.atomic.AtomicInteger
-import scala.concurrent.duration.DurationInt
-import scala.concurrent.{Await, Future, Promise}
+import scala.concurrent.{Future, Promise}
 import scala.util.Try
 
 case class TweetData(id: String, text: String)
@@ -16,24 +14,6 @@ case class TweetResult(data: Option[TweetData])
 
 trait TwitterHandler extends Logging { self: InvoiceMonitor =>
   import system.dispatcher
-
-  private lazy val shillCounter: AtomicInteger = {
-    // set shill counter based off db
-    val f = invoiceDAO.numCompleted()
-    val res = Try(Await.result(f, 60.seconds))
-    new AtomicInteger(res.getOrElse(0))
-  }
-
-  val uriErrorString = "Error: try again"
-  var uri: String = uriErrorString
-
-  def setURI(): Future[Unit] = {
-    lnd.getInfo.map { info =>
-      val torAddrOpt = info.uris.find(_.contains(".onion"))
-
-      uri = torAddrOpt.getOrElse(info.uris.head)
-    }
-  }
 
   implicit val tweetDataReads: Reads[TweetData] = Json.reads[TweetData]
   implicit val tweetResultReads: Reads[TweetResult] = Json.reads[TweetResult]
@@ -82,12 +62,6 @@ trait TwitterHandler extends Logging { self: InvoiceMonitor =>
       message: String,
       txId: DoubleSha256DigestBE): Future[TweetData] =
     FutureUtil.makeAsync { () =>
-      // Every 15th OP_RETURN we shill
-      val count = shillCounter.getAndIncrement()
-      if (count % 15 == 0 && count != 0) {
-        shillTweet()
-      }
-
       val usedMessage = config.censorMessage(message)
 
       val tweet =
@@ -101,22 +75,4 @@ trait TwitterHandler extends Logging { self: InvoiceMonitor =>
 
       sendTweet(tweet)
     }.flatten
-
-  private def shillTweet(): Future[Unit] = {
-    if (uri != uriErrorString) {
-      val tweet =
-        s"""
-           |Like OP_RETURN Bot?
-           |
-           |Consider connecting and opening a lightning channel!
-           |
-           |$uri
-           |""".stripMargin
-
-      sendTweet(tweet).map(_ => ()).recover { case ex =>
-        logger.error(s"Failed to send shill tweet: ${ex.getMessage}")
-        ()
-      }
-    } else Future.unit
-  }
 }
