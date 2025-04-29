5de50c137bcd366535c254c1f7032e75ab8211f1
benthecarman <benthecarman@live.com>
2025-04-29 13:48:37 -0500

    Remove unneeded test

--- test/unit/UnitSpec.scala
+++ test/unit/UnitSpec.scala
@@ -39,26 +39,6 @@ class UnitSpec extends PlaySpec {
       request.message must equal("foo")
     }
 
-    "show errors when applied unsuccessfully" in {
-      // Pass in a negative price that fails the constraints...
-      val data = Map(
-        "message" -> "this is over 80 characters _____________________________________________________________________")
-
-      // ...and binding the form will show errors.
-      val errorForm = Forms.opReturnRequestForm.bind(data)
-      // You can then get the data out and test it.
-      val listOfErrors = errorForm.errors
-
-      // Note that the FormError's key is the field it was bound to.
-      // If there is no key, then it is a "global error".
-      val formError: FormError = listOfErrors.head
-      formError.key must equal("message")
-
-      // In this case, we don't have any global errors -- they're caused
-      // when a constraint on the form itself fails.
-      errorForm.hasGlobalErrors mustBe false
-    }
-
   }
 
 }
